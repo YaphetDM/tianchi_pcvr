598f675481b608142a983edc94c5a9d65da0454e
xxyu2 <xxyu2@iflytek.com>
2018-03-08 17:42:35 +0800

    xxxx

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #coding:utf-8
-from .utils import input_data
+from utils import input_data
 import xgboost as xgb
-data_path = ''
+if __name__ == '__main__':
+    path = 'data/train.txt'
+    train, valuate, test, featmap = input_data(path)
+    for i in featmap:
+        print(i, featmap[i])
+    for each in train.features()[0:5]:
+        print(len(each))
